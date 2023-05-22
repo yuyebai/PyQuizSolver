6a00082a7b73871c607a37e54c22f33d2b210871
yuyebai bai <2849593864@qq.com>
2023-05-22 12:03:17 +0000

    four

---
+++ Quiz_four.py
@@ -0,0 +1,5 @@
+#利用collections库的Counter方法统计字符串每个单词出现的次数"kjalfj;ldsjafl;hdsllfdhg;lahfbl;hl;ahlf;h"
+from collections import Counter
+a = "kjalfj;ldsjafl;hdsllfdhg;lahfbl;hl;ahlf;h"
+res = Counter(a)
+print(res)

497437c498004cb212f833660151955678f406bd
yuyebai bai <2849593864@qq.com>
2023-05-22 12:09:23 +0000

    five1

--- Quiz_five.py
+++ Quiz_five.py
@@ -3,7 +3,7 @@ import re
 a = "not 404 found 张三 99 深圳"
 list = a.split(" ")
 print(list)
-res=tr.findall('\d+[a-zA-Z]+',a)
+res=re.findall('\d+|[a-zA-Z]+',a)
 for i in res:
     if i in list:
         list.remove(i)
